edf77000a4b3090bf7b5fb8a659e581294969388
John Ky <newhoggy@gmail.com>
2017-08-11 15:54:40 +1000

    Eliminate tuple for performance

--- test/HaskellWorks/Data/Excess/MinMaxExcess1Spec.hs
+++ test/HaskellWorks/Data/Excess/MinMaxExcess1Spec.hs
@@ -3,13 +3,15 @@
 
 module HaskellWorks.Data.Excess.MinMaxExcess1Spec (spec) where
 
+import Data.Word
+import HaskellWorks.Data.Bits.Word
+import HaskellWorks.Data.Excess.MinMaxExcess1
+import HaskellWorks.Data.Excess.Triplet
+import HaskellWorks.Data.Naive
+import Test.Hspec
+import Test.QuickCheck
+
 import qualified Data.Vector.Storable                             as DVS
-import           Data.Word
-import           HaskellWorks.Data.Bits.Word
-import           HaskellWorks.Data.Excess.MinMaxExcess1
-import           HaskellWorks.Data.Naive
-import           Test.Hspec
-import           Test.QuickCheck
 
 {-# ANN module ("HLint: Ignore Redundant do" :: String) #-}
 {-# ANN module ("HLint: Ignore Reduce duplication"  :: String) #-}
@@ -19,120 +21,120 @@ spec = describe "HaskellWorks.Data.Excess.MinMaxExcess1Spec" $ do
   describe "For Word8" $ do
     it "Excess should be between min excess and max excess" $
       property $ \(w :: Word8) ->
-        let (minE, e, maxE) = minMaxExcess1 w in
+        let Triplet minE e maxE = minMaxExcess1 w in
         minE <= e && e <= maxE
     it "minE2 == minE0 `min` (minE1 + e0)" $
       property $ \(w0 :: Word8, w1 :: Word8) ->
         let w2 = leConcat w0 w1 in
-        let (minE0, e0, _) = minMaxExcess1 w0 in
-        let (minE1, _ , _) = minMaxExcess1 w1 in
-        let (minE2, _ , _) = minMaxExcess1 w2 in
+        let Triplet minE0 e0 _ = minMaxExcess1 w0 in
+        let Triplet minE1 _  _ = minMaxExcess1 w1 in
+        let Triplet minE2 _  _ = minMaxExcess1 w2 in
         minE2 == minE0 `min` (minE1 + e0)
     it "maxE2 == maxE0 `max` (maxE1 + e0)" $
       property $ \(w0 :: Word8, w1 :: Word8) ->
         let w2 = leConcat w0 w1 in
-        let (_, e0, maxE0) = minMaxExcess1 w0 in
-        let (_, _ , maxE1) = minMaxExcess1 w1 in
-        let (_, _ , maxE2) = minMaxExcess1 w2 in
+        let Triplet _ e0 maxE0 = minMaxExcess1 w0 in
+        let Triplet _ _  maxE1 = minMaxExcess1 w1 in
+        let Triplet _ _  maxE2 = minMaxExcess1 w2 in
         maxE2 == maxE0 `max` (maxE1 + e0)
     it "minE2 == minE0 `min` (minE1 + e0) via vector" $
       property $ \(w0 :: Word8, w1 :: Word8) ->
         let w2 = DVS.fromList [w0, w1] in
-        let (minE0, e0, _) = minMaxExcess1 w0 in
-        let (minE1, _ , _) = minMaxExcess1 w1 in
-        let (minE2, _ , _) = minMaxExcess1 w2 in
+        let Triplet minE0 e0 _ = minMaxExcess1 w0 in
+        let Triplet minE1 _  _ = minMaxExcess1 w1 in
+        let Triplet minE2 _  _ = minMaxExcess1 w2 in
         minE2 == minE0 `min` (minE1 + e0)
     it "maxE2 == maxE0 `max` (maxE1 + e0) via vector" $
       property $ \(w0 :: Word8, w1 :: Word8) ->
         let w2 = DVS.fromList [w0, w1] in
-        let (_, e0, maxE0) = minMaxExcess1 w0 in
-        let (_, _ , maxE1) = minMaxExcess1 w1 in
-        let (_, _ , maxE2) = minMaxExcess1 w2 in
+        let Triplet _ e0 maxE0 = minMaxExcess1 w0 in
+        let Triplet _ _  maxE1 = minMaxExcess1 w1 in
+        let Triplet _ _  maxE2 = minMaxExcess1 w2 in
         maxE2 == maxE0 `max` (maxE1 + e0)
   describe "For Word16" $ do
     it "Excess should be between min excess and max excess" $
       property $ \(w :: Word16) ->
-        let (minE, e, maxE) = minMaxExcess1 w in
+        let Triplet minE e maxE = minMaxExcess1 w in
         minE <= e && e <= maxE
     it "minE2 == minE0 `min` (minE1 + e0)" $
       property $ \(w0 :: Word16, w1 :: Word16) ->
         let w2 = leConcat w0 w1 in
-        let (minE0, e0, _) = minMaxExcess1 w0 in
-        let (minE1, _ , _) = minMaxExcess1 w1 in
-        let (minE2, _ , _) = minMaxExcess1 w2 in
+        let Triplet minE0 e0 _ = minMaxExcess1 w0 in
+        let Triplet minE1 _  _ = minMaxExcess1 w1 in
+        let Triplet minE2 _  _ = minMaxExcess1 w2 in
         minE2 == minE0 `min` (minE1 + e0)
     it "maxE2 == maxE0 `max` (maxE1 + e0)" $
       property $ \(w0 :: Word16, w1 :: Word16) ->
         let w2 = leConcat w0 w1 in
-        let (_, e0, maxE0) = minMaxExcess1 w0 in
-        let (_, _ , maxE1) = minMaxExcess1 w1 in
-        let (_, _ , maxE2) = minMaxExcess1 w2 in
+        let Triplet _ e0 maxE0 = minMaxExcess1 w0 in
+        let Triplet _ _  maxE1 = minMaxExcess1 w1 in
+        let Triplet _ _  maxE2 = minMaxExcess1 w2 in
         maxE2 == maxE0 `max` (maxE1 + e0)
     it "minE2 == minE0 `min` (minE1 + e0) via vector" $
       property $ \(w0 :: Word16, w1 :: Word16) ->
         let w2 = DVS.fromList [w0, w1] in
-        let (minE0, e0, _) = minMaxExcess1 w0 in
-        let (minE1, _ , _) = minMaxExcess1 w1 in
-        let (minE2, _ , _) = minMaxExcess1 w2 in
+        let Triplet minE0 e0 _ = minMaxExcess1 w0 in
+        let Triplet minE1 _  _ = minMaxExcess1 w1 in
+        let Triplet minE2 _  _ = minMaxExcess1 w2 in
         minE2 == minE0 `min` (minE1 + e0)
     it "maxE2 == maxE0 `max` (maxE1 + e0) via vector" $
       property $ \(w0 :: Word16, w1 :: Word16) ->
         let w2 = DVS.fromList [w0, w1] in
-        let (_, e0, maxE0) = minMaxExcess1 w0 in
-        let (_, _ , maxE1) = minMaxExcess1 w1 in
-        let (_, _ , maxE2) = minMaxExcess1 w2 in
+        let Triplet _ e0 maxE0 = minMaxExcess1 w0 in
+        let Triplet _ _  maxE1 = minMaxExcess1 w1 in
+        let Triplet _ _  maxE2 = minMaxExcess1 w2 in
         maxE2 == maxE0 `max` (maxE1 + e0)
     describe "For Word32" $ do
       it "Excess should be between min excess and max excess" $
         property $ \(w :: Word32) ->
-          let (minE, e, maxE) = minMaxExcess1 w in
+          let Triplet minE e maxE = minMaxExcess1 w in
           minE <= e && e <= maxE
       it "minE2 == minE0 `min` (minE1 + e0)" $
         property $ \(w0 :: Word32, w1 :: Word32) ->
           let w2 = leConcat w0 w1 in
-          let (minE0, e0, _) = minMaxExcess1 w0 in
-          let (minE1, _ , _) = minMaxExcess1 w1 in
-          let (minE2, _ , _) = minMaxExcess1 w2 in
+          let Triplet minE0 e0 _ = minMaxExcess1 w0 in
+          let Triplet minE1 _  _ = minMaxExcess1 w1 in
+          let Triplet minE2 _  _ = minMaxExcess1 w2 in
           minE2 == minE0 `min` (minE1 + e0)
       it "maxE2 == maxE0 `max` (maxE1 + e0)" $
         property $ \(w0 :: Word32, w1 :: Word32) ->
           let w2 = leConcat w0 w1 in
-          let (_, e0, maxE0) = minMaxExcess1 w0 in
-          let (_, _ , maxE1) = minMaxExcess1 w1 in
-          let (_, _ , maxE2) = minMaxExcess1 w2 in
+          let Triplet _ e0 maxE0 = minMaxExcess1 w0 in
+          let Triplet _ _  maxE1 = minMaxExcess1 w1 in
+          let Triplet _ _  maxE2 = minMaxExcess1 w2 in
           maxE2 == maxE0 `max` (maxE1 + e0)
       it "minE2 == minE0 `min` (minE1 + e0) via vector" $
         property $ \(w0 :: Word32, w1 :: Word32) ->
           let w2 = DVS.fromList [w0, w1] in
-          let (minE0, e0, _) = minMaxExcess1 w0 in
-          let (minE1, _ , _) = minMaxExcess1 w1 in
-          let (minE2, _ , _) = minMaxExcess1 w2 in
+          let Triplet minE0 e0 _ = minMaxExcess1 w0 in
+          let Triplet minE1 _  _ = minMaxExcess1 w1 in
+          let Triplet minE2 _  _ = minMaxExcess1 w2 in
           minE2 == minE0 `min` (minE1 + e0)
       it "maxE2 == maxE0 `max` (maxE1 + e0) via vector" $
         property $ \(w0 :: Word32, w1 :: Word32) ->
           let w2 = DVS.fromList [w0, w1] in
-          let (_, e0, maxE0) = minMaxExcess1 w0 in
-          let (_, _ , maxE1) = minMaxExcess1 w1 in
-          let (_, _ , maxE2) = minMaxExcess1 w2 in
+          let Triplet _ e0 maxE0 = minMaxExcess1 w0 in
+          let Triplet _ _  maxE1 = minMaxExcess1 w1 in
+          let Triplet _ _  maxE2 = minMaxExcess1 w2 in
           maxE2 == maxE0 `max` (maxE1 + e0)
   describe "For Word64" $ do
     it "Excess should be between min excess and max excess" $
       property $ \(w :: Word64) ->
-        let (minE, e, maxE) = minMaxExcess1 w in
+        let Triplet minE e maxE = minMaxExcess1 w in
         minE <= e && e <= maxE
     it "minE2 == minE0 `min` (minE1 + e0) via vector" $
       property $ \(w0 :: Word64, w1 :: Word64) ->
         let w2 = DVS.fromList [w0, w1] in
-        let (minE0, e0, _) = minMaxExcess1 w0 in
-        let (minE1, _ , _) = minMaxExcess1 w1 in
-        let (minE2, _ , _) = minMaxExcess1 w2 in
+        let Triplet minE0 e0 _ = minMaxExcess1 w0 in
+        let Triplet minE1 _  _ = minMaxExcess1 w1 in
+        let Triplet minE2 _  _ = minMaxExcess1 w2 in
         minE2 == minE0 `min` (minE1 + e0)
     it "maxE2 == maxE0 `max` (maxE1 + e0) via vector" $
       property $ \(w0 :: Word64, w1 :: Word64) ->
         let w2 = DVS.fromList [w0, w1] in
-        let (_, e0, maxE0) = minMaxExcess1 w0 in
-        let (_, _ , maxE1) = minMaxExcess1 w1 in
-        let (_, _ , maxE2) = minMaxExcess1 w2 in
+        let Triplet _ e0 maxE0 = minMaxExcess1 w0 in
+        let Triplet _ _  maxE1 = minMaxExcess1 w1 in
+        let Triplet _ _  maxE2 = minMaxExcess1 w2 in
         maxE2 == maxE0 `max` (maxE1 + e0)
   describe "Equivalent to native implementation" $ do
     it "For Word8" $ do
