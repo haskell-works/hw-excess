86455307a41fde05de2cdc1af981d95bbef260d7
John Ky <newhoggy@gmail.com>
2020-01-21 20:26:05 +1100

    Use generate instead of constructN

--- src/HaskellWorks/Data/Excess/Internal/Partial/Table.hs
+++ src/HaskellWorks/Data/Excess/Internal/Partial/Table.hs
@@ -19,39 +19,39 @@ import qualified Data.Vector.Storable                       as DVS
 import qualified HaskellWorks.Data.Excess.Internal.Triplet8 as T8
 
 tableWord8Excess0Lo :: DVS.Vector Int8
-tableWord8Excess0Lo = DVS.constructN (256 * 9) go
-  where go :: DVS.Vector Int8 -> Int8
-        go u =  let ui = DVS.length u in T8.triplet8Lo (genWord8Excess0 (ui `div` 256) (fromIntegral ui))
+tableWord8Excess0Lo = DVS.generate (256 * 9) go
+  where go :: Int -> Int8
+        go i = T8.triplet8Lo (genWord8Excess0 (i `div` 256) (fromIntegral i))
 {-# NOINLINE tableWord8Excess0Lo #-}
 
 tableWord8Excess0Ex :: DVS.Vector Int8
-tableWord8Excess0Ex = DVS.constructN (256 * 9) go
-  where go :: DVS.Vector Int8 -> Int8
-        go u =  let ui = DVS.length u in T8.triplet8Ex (genWord8Excess0 (ui `div` 256) (fromIntegral ui))
+tableWord8Excess0Ex = DVS.generate (256 * 9) go
+  where go :: Int -> Int8
+        go i = T8.triplet8Ex (genWord8Excess0 (i `div` 256) (fromIntegral i))
 {-# NOINLINE tableWord8Excess0Ex #-}
 
 tableWord8Excess0Hi :: DVS.Vector Int8
-tableWord8Excess0Hi = DVS.constructN (256 * 9) go
-  where go :: DVS.Vector Int8 -> Int8
-        go u =  let ui = DVS.length u in T8.triplet8Hi (genWord8Excess0 (ui `div` 256) (fromIntegral ui))
+tableWord8Excess0Hi = DVS.generate (256 * 9) go
+  where go :: Int -> Int8
+        go i = T8.triplet8Hi (genWord8Excess0 (i `div` 256) (fromIntegral i))
 {-# NOINLINE tableWord8Excess0Hi #-}
 
 tableWord8Excess1Lo :: DVS.Vector Int8
-tableWord8Excess1Lo = DVS.constructN (256 * 9) go
-  where go :: DVS.Vector Int8 -> Int8
-        go u =  let ui = DVS.length u in T8.triplet8Lo (genWord8Excess1 (ui `div` 256) (fromIntegral ui))
+tableWord8Excess1Lo = DVS.generate (256 * 9) go
+  where go :: Int -> Int8
+        go i = T8.triplet8Lo (genWord8Excess1 (i `div` 256) (fromIntegral i))
 {-# NOINLINE tableWord8Excess1Lo #-}
 
 tableWord8Excess1Ex :: DVS.Vector Int8
-tableWord8Excess1Ex = DVS.constructN (256 * 9) go
-  where go :: DVS.Vector Int8 -> Int8
-        go u =  let ui = DVS.length u in T8.triplet8Ex (genWord8Excess1 (ui `div` 256) (fromIntegral ui))
+tableWord8Excess1Ex = DVS.generate (256 * 9) go
+  where go :: Int -> Int8
+        go i = T8.triplet8Ex (genWord8Excess1 (i `div` 256) (fromIntegral i))
 {-# NOINLINE tableWord8Excess1Ex #-}
 
 tableWord8Excess1Hi :: DVS.Vector Int8
-tableWord8Excess1Hi = DVS.constructN (256 * 9) go
-  where go :: DVS.Vector Int8 -> Int8
-        go u =  let ui = DVS.length u in T8.triplet8Hi (genWord8Excess1 (ui `div` 256) (fromIntegral ui))
+tableWord8Excess1Hi = DVS.generate (256 * 9) go
+  where go :: Int -> Int8
+        go i = T8.triplet8Hi (genWord8Excess1 (i `div` 256) (fromIntegral i))
 {-# NOINLINE tableWord8Excess1Hi #-}
 
 genWord8Excess0 :: Int -> Word8 -> Triplet8
